3cdb06e583d8cd3348eda0bb569d047efb054703
nadir budhwani <deniro0@yahoo.com>
2016-01-12 13:48:49 -0500

    string interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -1,2 +1,6 @@
-tip = 0.2
-puts "A good tip for a $55 meal is #{55 * tip}"
+# tip = 0.2
+# puts "A good tip for a $55 meal is #{55 * tip}"
+
+# puts "There are " + 7.to_s + " days in the week."
+
+puts "45628 multiplied by 7839 is #{45628 * 7839}"
